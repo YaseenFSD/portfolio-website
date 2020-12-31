d10939d0e8e0d4fc3d5e19b2c92f2eb952803fe4
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-12-31 14:04:54 -0500

    Initial particles completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react"
+// import useWindowSize from "./custom-hooks/useWindowSize"
+import Particles from "react-particles-js"
+import particleProps from "./particles.json"
 import './App.css';
 
 function App() {
+  // const { windowWidth, windowHeight } = useWindowSize()
+  const [isDarkTheme, setDarkTheme] = useState(true)
+  const lightBG = {
+    ...particleProps,
+    background: {
+      color: {
+        height: "100%",
+        value: "#FFF"
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles params={isDarkTheme ? particleProps : lightBG} />
+
     </div>
   );
 }
