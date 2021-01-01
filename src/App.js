bd6305b960d7d2c1748e8b59aebf1426869d71fc
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-12-31 22:20:58 -0500

    toggle dark mode now works correctly with styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import { useState, useEffect } from "react"
-// import useWindowSize from "./custom-hooks/useWindowSize"
+import { useState } from "react"
 import Particles from "react-particles-js"
 import particlesDark from "./particlesDark.json"
 import particlesLight from "./particlesLight.json"
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./components/styles/Themes";
+import { Button, Wrapper } from "./components/styles/CustomStyles"
 import './App.css';
 
-function App() {
-  const [isDarkTheme, setDarkTheme] = useState(false)
 
-  return (
+
+function App() {
+  const [theme, setTheme] = useState("dark")
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light")
+  }
+
+  return (<ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
     <div className="App">
-        <Particles params={isDarkTheme ? particlesDark : particlesLight} />
-        <div className="projects-container">hi
-        hi</div>
+      <Particles params={theme === "light" ? particlesLight : particlesDark} />
+      <Wrapper className="main-container" >
+        {/* <Wrapper theme={theme} className={`main-container ${theme === "dark" ? "dark-theme" : ""}`}> */}
+
+        <Button onClick={themeToggler}>Click here</Button>
+        Test
+        </Wrapper>
     </div>
+  </ThemeProvider>
   );
 }
 
---
+++ src/components/styles/Themes.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    body: '#FFF',
+    text: '#000',
+    toggleBorder: '#FFF',
+}
+export const darkTheme = {
+    body: '#363537',
+    background: "000",
+    text: '#FFF',
+    toggleBorder: '#6B8096',
+}
