61571d1a395e7a880e8420143dc172818b46ff86
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2021-01-03 16:01:16 -0500

    Scrollbar now changes with the theme

--- src/App.js
+++ src/App.js
@@ -6,126 +6,142 @@ import { darkTheme, lightTheme } from "./components/styles/Themes";
 import MyAppBar from "./components/app-bar/MyAppBar"
 import { ThemeProvider } from "@material-ui/styles"
 import { createMuiTheme } from "@material-ui/core/styles"
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+
+const useStyles = makeStyles(({
+  mainContainer: {
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: (props => {
+        console.log(props)
+        console.log(props.palette.primary)
+        return props.palette.primary.main
+      }),
+      borderRadius: "20px"
+    }
+  }
+}))
 
 
 function App() {
   const [theme, setTheme] = useState("dark")
   const currentTheme = createMuiTheme(theme === "light" ? lightTheme : darkTheme)
+  const classes = useStyles(currentTheme)
+  // console.log(currentTheme)
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light")
   }
 
   return (<ThemeProvider theme={currentTheme} >
     <div className="App">
-      <MyAppBar themeToggler={themeToggler}/>
+      <MyAppBar themeToggler={themeToggler} />
       <Particles params={theme === "light" ? particlesLight : particlesDark} />
-      <div className="main-container" >
+      <div className={`main-container ${classes.mainContainer}`} >
         Scroll Test
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         Scroll Test
-        <div style={{height: "200px"}}></div>
+        <div style={{ height: "200px" }}></div>
         {/* // ! No content put after this div will show on page properly */}
-        </div>
+      </div>
     </div>
   </ThemeProvider>
   );
